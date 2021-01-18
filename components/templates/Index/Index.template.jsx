55b1c4ef1a61649337abdf45bb783d797b895562
vctorChamizo <victor.chamizo96@gmail.com>
2021-01-18 13:29:39 +0100

    feat: index module

--- components/templates/Index/Index.template.jsx
+++ components/templates/Index/Index.template.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { SCIndex } from "./Index.styled";
 
+import { database } from "../../../lib";
+
 export default function IndexTemplate() {
-  return <SCIndex>Index</SCIndex>;
+  const [petList, setPetList] = useState([]);
+  const [indexCarrousel, setIndexCarrousel] = useState(0);
+
+  useEffect(() => {
+    const pets = database
+      .ref("/pets")
+      .once("value")
+      .then((snapshot) => {
+        setPetList(snapshot.val());
+      });
+  }, []);
+
+  const handleClickElement = () => {};
+
+  const handleClickButton = () => {};
+
+  const hanldeClickArrow = (direction) => {};
+
+  return <SCIndex>{petList.map((element) => element.name)}</SCIndex>;
 }
